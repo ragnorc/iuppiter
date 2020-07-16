895d6b7b63e2fee49751fd2f495351594e3cef9c
Ragnor Comerford <hello@ragnor.co>
2020-07-16 15:06:40 +0200

    Try fix.

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ from prefect.agent import Agent
 from flows import spot_flow, fetch_daily_power_spot_flow
 
 
-spot_flow.storage = GitHub(repo="ragnorc/iuppiter", path="src/flows.py")
+#spot_flow.storage = GitHub(repo="ragnorc/iuppiter", path="src/flows.py")
 fetch_daily_power_spot_flow.storage = GitHub(repo="ragnorc/iuppiter", path="src/flows.py")
 #storage.add_flow(spot_flow)
 #storage.add_flow(fetch_daily_power_spot_flow)
@@ -17,6 +17,6 @@ fetch_daily_power_spot_flow.storage = GitHub(repo="ragnorc/iuppiter", path="src/
 #spot_flow.storage = storage
 #fetch_daily_power_spot_flow.storage = storage
 #print(storage.flows)
-spot_flow.register(project_name="iuppiter", build=False)
+#spot_flow.register(project_name="iuppiter", build=False)
 fetch_daily_power_spot_flow.register(project_name="iuppiter", build=False)
 print("ran docker")

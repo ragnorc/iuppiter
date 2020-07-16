7e94812afc734e9bfd1d8a95a869c7a2c41e1287
Ragnor Comerford <hello@ragnor.co>
2020-07-16 15:03:59 +0200

    Try fix.

--- src/main.py
+++ src/main.py
@@ -8,14 +8,14 @@ from prefect.agent import Agent
 from flows import spot_flow, fetch_daily_power_spot_flow
 
 
-storage = GitHub(repo="ragnorc/iuppiter", path="src/flows.py")
+spot_flow.storage = GitHub(repo="ragnorc/iuppiter", path="src/flows.py")
+fetch_daily_power_spot_flow.storage = GitHub(repo="ragnorc/iuppiter", path="src/flows.py")
+#storage.add_flow(spot_flow)
+#storage.add_flow(fetch_daily_power_spot_flow)
+#storage = storage.build()
 
-storage.add_flow(spot_flow)
-storage.add_flow(fetch_daily_power_spot_flow)
-storage = storage.build()
-
-spot_flow.storage = storage
-fetch_daily_power_spot_flow.storage = storage
+#spot_flow.storage = storage
+#fetch_daily_power_spot_flow.storage = storage
 #print(storage.flows)
 spot_flow.register(project_name="iuppiter", build=False)
 fetch_daily_power_spot_flow.register(project_name="iuppiter", build=False)

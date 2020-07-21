61e30854c790e06bf7d9763521b271930a85d9a3
Ragnor Comerford <hello@ragnor.co>
2020-07-21 14:24:11 +0200

    Fix timezone.

--- src/power_daily_spot_flow.py
+++ src/power_daily_spot_flow.py
@@ -9,13 +9,18 @@ import requests
 import xmltodict
 import os
 from tasks import write_to_db
+from dateutil import tz
 
 
-def fetch_daily_spot():
+def fetch_daily_spot(day):
     print("Fetching daily spot price")
-    res = requests.get("https://transparency.entsoe.eu/api?documentType=A44&securityToken=dcce50af-fffe-43ee-b8b8-b2a0bdc35d6f&in_Domain=10Y1001A1001A82H&out_Domain=10Y1001A1001A82H", params={'periodStart': (datetime.datetime.today()- datetime.timedelta(days=2)).strftime("%Y%m%d")+"2200", 'periodEnd': (datetime.datetime.today()- datetime.timedelta(days=1)).strftime("%Y%m%d")+"2200"})
+    start = datetime.datetime(day.year, day.month, day.day)
+    end = start + datetime.timedelta(1)
+    start_utc = datetime.datetime(day.year, day.month, day.day, tzinfo=tz.gettz("Europe/Berlin")).astimezone(tz.tzutc())
+    end_utc = start_utc + datetime.timedelta(1)
+    res = requests.get("https://transparency.entsoe.eu/api?documentType=A44&securityToken=dcce50af-fffe-43ee-b8b8-b2a0bdc35d6f&in_Domain=10Y1001A1001A82H&out_Domain=10Y1001A1001A82H", params={'periodStart': start_utc.strftime("%Y%m%d%H%M"), 'periodEnd': end_utc.strftime("%Y%m%d%H%M")})
     prices = [ point["price.amount"]  for point in xmltodict.parse(res.content)["Publication_MarketDocument"]["TimeSeries"]["Period"]["Point"]]
-    dates = pd.date_range((datetime.datetime.today()- datetime.timedelta(days=2)).strftime("%Y%m%d"),(datetime.datetime.today()- datetime.timedelta(days=1)).strftime("%Y%m%d"), freq="H", closed="left")
+    dates = pd.date_range(start, end, freq="H", closed="left")
     df = pd.DataFrame()
     df["price"] = prices
     df["datetime"] = dates
@@ -23,5 +28,6 @@ def fetch_daily_spot():
     return df
 
 
-daily_spot = fetch_daily_spot()
+daily_spot = fetch_daily_spot(datetime.datetime.today()-datetime.timedelta(days=1))
+print(daily_spot)
 write_to_db([{**item, "datetime": item['datetime'].replace('Z', '')} for item in json.loads(daily_spot.to_json(orient='records', date_format="iso"))], "PowerSpot","power_spot_datetime", "datetime")

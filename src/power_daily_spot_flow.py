ff7260b5fe091aa6c4e69317d51bfc994f708310
Ragnor Comerford <hello@ragnor.co>
2020-07-21 12:59:43 +0200

    Refactor flows.

---
+++ src/power_daily_spot_flow.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import json
+import datetime
+import matplotlib.pyplot as plt
+from faunadb import query as q
+from faunadb.objects import Ref
+from faunadb.client import FaunaClient
+import requests
+import xmltodict
+import os
+from tasks import write_to_db
+
+
+def fetch_daily_spot():
+    print("Fetching daily spot price")
+    res = requests.get("https://transparency.entsoe.eu/api?documentType=A44&securityToken=dcce50af-fffe-43ee-b8b8-b2a0bdc35d6f&in_Domain=10Y1001A1001A82H&out_Domain=10Y1001A1001A82H", params={'periodStart': (datetime.datetime.today()- datetime.timedelta(days=2)).strftime("%Y%m%d")+"2200", 'periodEnd': (datetime.datetime.today()- datetime.timedelta(days=1)).strftime("%Y%m%d")+"2200"})
+    prices = [ point["price.amount"]  for point in xmltodict.parse(res.content)["Publication_MarketDocument"]["TimeSeries"]["Period"]["Point"]]
+    dates = pd.date_range((datetime.datetime.today()- datetime.timedelta(days=2)).strftime("%Y%m%d"),(datetime.datetime.today()- datetime.timedelta(days=1)).strftime("%Y%m%d"), freq="H", closed="left")
+    df = pd.DataFrame()
+    df["price"] = prices
+    df["datetime"] = dates
+    print(df)
+    return df
+
+
+daily_spot = fetch_daily_spot()
+print([{**item, "datetime": item['datetime'].replace('Z', '')} for item in json.loads(daily_spot.to_json(orient='records', date_format="iso"))], "PowerSpot","power_spot_datetime", "datetime")
--- src/power_spot_forecast_flow.py
+++ src/power_spot_forecast_flow.py
@@ -10,16 +10,10 @@ from faunadb.client import FaunaClient
 import requests
 import xmltodict
 import os
-
-
-
-def fetch_daily_spot():
-    res = requests.get("https://transparency.entsoe.eu/api?documentType=A44&securityToken=dcce50af-fffe-43ee-b8b8-b2a0bdc35d6f&in_Domain=10Y1001A1001A82H&out_Domain=10Y1001A1001A82H", params={'periodStart': (datetime.datetime.today()- datetime.timedelta(days=2)).strftime("%Y%m%d")+"2200", 'periodEnd': (datetime.datetime.today()- datetime.timedelta(days=1)).strftime("%Y%m%d")+"2200"})
-    print(str([ point["price.amount"]  for point in xmltodict.parse(res.content)["Publication_MarketDocument"]["TimeSeries"]["Period"]["Point"]]))
-
+from tasks import write_to_db
 
 def fetch_historical_spot():
-    client = FaunaClient(secret="fnADwBbPWHACBcWfAJOyZUHjoJ5cMFuZu3k9B2NO")
+    client = FaunaClient(secret=os.environ["FAUNA_SECRET"])
     df = pd.DataFrame(client.query(q.map_expr(
     lambda x: {"ds": q.select(["data","datetime"],q.get(x)), "y": q.select(["data","price"],q.get(x))},
         q.paginate(q.documents(q.collection('PowerSpot')), size=100000)
@@ -43,27 +37,8 @@ def predict_spot(model, future):
     #plt.show()
     return forecast
 
-def write_to_db(items, collection, index, unique_key):
-    def chunk(seq, size):
-        return (seq[pos:pos + size] for pos in range(0, len(seq), size))
-    client = FaunaClient(secret=os.environ["FAUNA_SECRET"])
-    for items in chunk(items, 500):
-        print(len(items))
-        client.query(
-    q.map_expr(
-        lambda item:
-       q.call(
-    q.function('upsert'),
-    [collection,index, q.select(unique_key, item), item ],
-  ),
-      items
-        )
-    )
-
-
-fetch_daily_spot()
 historical_spot = fetch_historical_spot()
 trained_model = train_prophet(historical_spot)
 forecast = predict_spot(trained_model, pd.date_range(start=(historical_spot["ds"]).max(), end=historical_spot["ds"].max()+pd.offsets.DateOffset(years=6), freq="H").to_frame(index=False, name='ds'))
 print(forecast)
-write_to_db([{"datetime": item['ds'].replace('Z', ''), "price": item['yhat'], **item} for item in json.loads(forecast.to_json(orient='records', date_format="iso"))], "PowerSpotForecast","power_spot_forecast_datetime", "datetime")
+write_to_db([{**item, "datetime": item['ds'].replace('Z', ''), "price": item['yhat'] } for item in json.loads(forecast.to_json(orient='records', date_format="iso"))], "PowerSpotForecast","power_spot_forecast_datetime", "datetime")

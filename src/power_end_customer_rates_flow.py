4ac82a8c01becf135ca9b9edc43fecf8ac8ea0ec
Ragnor Comerford <hello@ragnor.co>
2020-08-03 13:58:00 +0400

    Fix scraper.

--- src/power_end_customer_rates_flow.py
+++ src/power_end_customer_rates_flow.py
@@ -22,18 +22,17 @@ def get_end_customer_rates_check24(zipcode, consumption, csv=False):
     #validate_zipcode(zipcode)
     #validate_consumption(consumption)
 
-    url = 'https://www.check24.de/strom/'
+    url = 'https://www.check24.de/strom/vergleich/check24/?pid=24&zipcode={}&totalconsumption={}&contractperiod_toggle=12&considerdiscounts=yes&maxbonusshare=yes&eco=no&eco_type=normal&priceguarantee=fixed_price&priceguarantee_months=12&companyevaluation_positive=yes&customertype=private&contractperiod=12&cancellationperiod=42&pagesize=500'.format(zipcode, consumption)
 
     chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_argument('--no-sandbox')
-    chrome_options.add_argument('--window-size=1420,1080')
-    chrome_options.add_argument('--headless')
-    chrome_options.add_argument('--disable-gpu')
+    #chrome_options.add_argument('--no-sandbox')
+    #chrome_options.add_argument('--window-size=1420,1080')
+    #chrome_options.add_argument('--headless')
+    #chrome_options.add_argument('--disable-gpu')
     driver = webdriver.Chrome(chrome_options=chrome_options)
     driver.get(url)
 
     time.sleep(2)
-    
 
 #    try:
 #        cookie_button = driver.find_element_by_class_name(
@@ -43,17 +42,23 @@ def get_end_customer_rates_check24(zipcode, consumption, csv=False):
 
 #    except ElementNotInteractableException:
 #        pass
+        
 
-    zipcode_input = driver.find_element_by_id('c24api_zipcode')
-    zipcode_input.clear()
-    zipcode_input.send_keys(zipcode)
+    #zipcode_input = driver.find_element_by_id('c24api_zipcode')
+    #zipcode_input.clear()
+    #zipcode_input.send_keys(zipcode)
+    #driver.execute_script("document.getElementById('c24api_zipcode').value="+str(zipcode))
 
-    consumption_input = driver.find_element_by_id('c24api_totalconsumption')
-    consumption_input.clear()
-    consumption_input.send_keys(consumption)
 
-    compare_button = driver.find_element_by_id('c24_calculate')
-    compare_button.click()
+    #consumption_input = driver.find_element_by_id('c24api_totalconsumption')
+    #driver.execute_script("document.getElementById('c24api_totalconsumption').value=''")
+    #consumption_input.clear()
+    #consumption_input.send_keys(consumption)
+    #driver.execute_script("document.getElementById('c24api_totalconsumption').value="+str(consumption))
+
+
+    #compare_button = driver.find_element_by_id('c24_calculate')
+    #compare_button.click()
 
     query_dt = datetime.utcnow()
 
@@ -61,86 +66,72 @@ def get_end_customer_rates_check24(zipcode, consumption, csv=False):
 
     # scoll down to the bottom of the page  and load additional results,
     # until every result is visible
-    try:
-        while True:
-            driver.execute_script("window.scrollTo(0, \
-             document.body.scrollHeight);")
 
-            load_add_res = driver.find_element_by_class_name(
-                'paginator__button')
-
-            load_add_res.click()
-            time.sleep(2)
-
-    except ElementNotInteractableException:
-        pass
-
-    finally:
-        driver.refresh()
-        provider_price_details = driver.find_elements_by_class_name(
+    driver.refresh()
+    provider_price_details = driver.find_elements_by_class_name(
             'ajax-pricelayer')
 
-        data = [provider.get_attribute('textContent').split()
+    data = [provider.get_attribute('textContent').split()
                 for provider in provider_price_details]
 
-        driver.quit()  # Close browser
+    driver.quit()  # Close browser
 
-        df = pd.DataFrame()
+    df = pd.DataFrame()
 
-        # Process Data and append to dataframe
-        print(data)
-        df['grossHourlyRate'] = [
+    # Process Data and append to dataframe
+    df['grossHourlyRate'] = [
             float(e[e.index('Arbeitspreis') + 5].replace(',', '.'))
             if 'Arbeitspreis' in e else None for e in data
         ]
 
-        df['grossBaseRate'] = [
+    df['grossBaseRate'] = [
             float(e[e.index('Grundpreis') + 3].replace(',', '.'))
             if 'Grundpreis' in e else None for e in data
 
         ]
 
-        df['netHourlyRate'] = [
+    df['netHourlyRate'] = [
             float(e[e.index('Arbeitspreis') + 6].replace(',', '.').replace('(', '').replace(')', ''))
             if 'Arbeitspreis' in e else None for e in data
         ]
 
-        df['netBaseRate'] = [
+    df['netBaseRate'] = [
             float(e[e.index('Grundpreis') + 4].replace(',', '.').replace('(', '').replace(')', ''))
             if 'Grundpreis' in e else None for e in data
 
         ]
 
-        df['appliesFrm'] = [
+    df['appliesFrm'] = [
             (datetime.strptime(e[e.index('gultig')+2],
                                '%d.%m.%Y.').date()).strftime('%Y-%m-%d')
             if 'gultig' in e else None for e in data
         ]
 
-        df['immediateBonus'] = [
+    df['immediateBonus'] = [
             float(e[e.index('Sofortbonus')+1].replace(',', '.'))
             if 'Sofortbonus' in e else None for e in data
         ]
 
-        df['newCustomerBonus'] = [
+    df['newCustomerBonus'] = [
             float(e[e.index('Neukundenbonus')+1].replace(',', '.'))
             if 'Neukundenbonus' in e else None for e in data
         ]
 
-        df['city'] = [data[1][5]]*len(data)
-        df['source'] = ['check24']*len(data)
-        df['sourceUrl'] = [url]*len(data)
-        df['productType'] = ['power']*len(data)
-        df['loadProfile'] = ['H0']*len(data)
-        df['maxContractDuration'] = [12]*len(data)
-        df['zipcode'] = [zipcode]*len(data)
-        df['consumption'] = [int(consumption)]*len(data)
-        df['date'] = datetime.today().date().isoformat()
-        df['rank'] = df.index
+    df['city'] = [data[1][5]]*len(data)
+    df['source'] = ['check24']*len(data)
+    df['sourceUrl'] = [url]*len(data)
+    df['productType'] = ['power']*len(data)
+    df['loadProfile'] = ['H0']*len(data)
+    df['maxContractDuration'] = [12]*len(data)
+    df['zipcode'] = [zipcode]*len(data)
+    df['consumption'] = [int(consumption)]*len(data)
+    df['date'] = datetime.today().date().isoformat()
+    df['rank'] = df.index
 
-        print(df)
-        return df
+    print(df)
+    return df
 
-for zipcode in [53604, 49090]:
-    end_customer_rates = get_end_customer_rates_check24(zipcode, 2500)
-    write_to_db(json.loads(end_customer_rates.to_json(orient='records')), "EndCustomerRate", "end_customer_rate_unique", ["date", "rank", "zipcode", "consumption", "loadProfile"])
+for zipcode, comsumption in [53604, 49090, 73312]:
+    for consumption in [1500 ,2500 ,4000 ,5500,10000]:
+        end_customer_rates = get_end_customer_rates_check24(zipcode, consumption)
+        write_to_db(json.loads(end_customer_rates.to_json(orient='records')), "EndCustomerRate", "end_customer_rate_unique", ["date", "rank", "zipcode", "consumption", "loadProfile"])

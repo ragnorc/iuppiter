86ae6977b92fcc44ff9a298b68cc1ee9d9a10105
Ragnor Comerford <hello@ragnor.co>
2020-08-05 00:14:18 +0400

    Add fixes.

--- src/power_daily_future_flow.py
+++ src/power_daily_future_flow.py
@@ -12,21 +12,28 @@ from dateutil import tz
 
 def fetch_eex_futures(product, type, date):
     data = requests.get("https://wrapapi.com/use/ragnorc/eex/futures/latest", params={"wrapAPIKey": "Vw52prEVYvxTYP7JN9MpNCIDVOKvf2iI", "product": '"/E.DE{}{}"'.format(type[0].upper(), product[0].upper()), "expirationDate": date.strftime("%Y/%m/%d"), "onDate": date.strftime("%Y/%m/%d")}).json()["data"]["output"]
-    return [{
+    results = []
+    for x in data:
+        if x["close"]:
+
+            result = {
         "date": date.date().isoformat(),
         #"closePrice": x["close"],
-        "base": x["close"] if type == "base" else "",
-        "peak": x["close"] if type == "peak" else "",
         "displayDate": datetime.datetime.strptime(x["gv.displaydate"], '%m/%d/%Y').date().isoformat(),
         "product": product,
         "year": datetime.datetime.strptime(x["gv.displaydate"], '%m/%d/%Y').year,
-        "quarter": int(math.ceil(datetime.datetime.strptime(x["gv.displaydate"], '%m/%d/%Y').month/3)) if product !="year" else "",
-        "month": datetime.datetime.strptime(x["gv.displaydate"], '%m/%d/%Y').month if product=="month" else "",
-        "original": {
+        "quarter": int(math.ceil(datetime.datetime.strptime(x["gv.displaydate"], '%m/%d/%Y').month/3)) if product !="year" else None,
+        "month": datetime.datetime.strptime(x["gv.displaydate"], '%m/%d/%Y').month if product=="month" else None,
+        
+         
+    } 
+            result[type] = x["close"]
+            result["original_"+type] = {
             **x
         },
-         
-    } for x in data if x["close"]]
+            results.append(result)
+    return results
+
 
 for product in ["month", "quarter", "year"]:
     for type in {"base", "peak"}:
